7da5f0d4f564fde379f951132261c33a4eefe93e
Antara055 <antara55.dgp10.1434@gmail.com>
2021-10-25 15:30:14 +0530

    Added : win or losse

--- src/com/bridgelab/GamblingSimulator.java
+++ src/com/bridgelab/GamblingSimulator.java
@@ -1,13 +1,23 @@
 package com.bridgelab;
+import java.util.Random;
 
 public class GamblingSimulator {
     int EVERYDAY_STAKE = 100;
     int EVERYDAY_BET = 1;
 
-    public static void main(String[] args) {
+        public void WinLosse() {
+            Random r = new Random();
+            int check = r.nextInt(2);
+            if (check == 0) {
+                System.out.println("WIN");
+            } else
+                System.out.println("LOSSE");
+        }
+        public static void main(String[] args) {
         GamblingSimulator gambler = new GamblingSimulator();
         System.out.println("WELCOME TO GAMBLING SIMULATOR");
         System.out.println("The user can start the day with: $" + gambler.EVERYDAY_STAKE);
         System.out.println("Minimum amount user can bet every game is: $" + gambler.EVERYDAY_BET);
+        gambler.WinLosse();
     }
 }
