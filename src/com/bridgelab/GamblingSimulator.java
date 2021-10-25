dad4e589377c9805399c3b3b99003da71290e513
Antara055 <antara55.dgp10.1434@gmail.com>
2021-10-25 16:26:39 +0530

    Added : After bet status

--- src/com/bridgelab/GamblingSimulator.java
+++ src/com/bridgelab/GamblingSimulator.java
@@ -6,31 +6,43 @@ public class GamblingSimulator {
     int EVERYGAME_BET = 1;
 
         public void WinLosse() {
+            int afterBet = 0;
             Random r = new Random();
-            int winCal=EVERYGAME_BET;
-            int looseCal=EVERYGAME_BET;
-            int percentage=EVERYDAY_STAKE/2;
-            int ifwin=percentage+EVERYDAY_STAKE;
-            int ifloose=EVERYDAY_STAKE-percentage;
+            for (int i = 1; i <= 20; i++) {
+                EVERYDAY_STAKE = afterBet;
+                int stakeCheck = EVERYDAY_STAKE;
+                int percentage = EVERYDAY_STAKE / 2;
+                int ifwin = percentage + EVERYDAY_STAKE;
+                int ifloose = EVERYDAY_STAKE - percentage;
 
-            while(EVERYDAY_STAKE>ifloose && EVERYDAY_STAKE<ifwin){
-                int check=r.nextInt(2);
-                if(check==0){
-                    //System.out.println("WIN");
-                    EVERYDAY_STAKE=EVERYDAY_STAKE+winCal;
-                    //System.out.println("changed amount after a win"+EVERYDAY_STAKE);
+                while (EVERYDAY_STAKE > ifloose && EVERYDAY_STAKE < ifwin) {
+                    int winLoose = r.nextInt(2);
+                    if (winLoose == 1) {
+                        //System.out.println("win");
+                        afterBet = EVERYDAY_STAKE + EVERYGAME_BET;
+                    } else {
+                        //System.out.println("loose");
+                        afterBet = EVERYDAY_STAKE - EVERYGAME_BET;
+                    }
+                    System.out.println("Bet amount for the  day " + i + " is:" + EVERYDAY_STAKE);
+
+
+                    EVERYDAY_STAKE = afterBet;
+                    //System.out.println("after play="+afterBet);
                 }
-                else{
-                    //System.out.println("LOSSE");
+                System.out.println("day " + i + " after the bet amount:" + EVERYDAY_STAKE);
 
-                    EVERYDAY_STAKE=EVERYDAY_STAKE-looseCal;
-                    //System.out.println("changed amount after a loose"+EVERYDAY_STAKE>);
-
+                if (stakeCheck < afterBet) {
+                    int profit = afterBet - stakeCheck;
+                    System.out.println("day " + i + " in profit:" + profit);
+                } else {
+                    int loose = stakeCheck - afterBet;
+                    System.out.println("day " + i + " in loose:" + loose);
                 }
-
             }
-            System.out.println("earning : "+EVERYDAY_STAKE);
         }
+
+
         public static void main(String[] args) {
         GamblingSimulator gambler = new GamblingSimulator();
         System.out.println("WELCOME TO GAMBLING SIMULATOR");
